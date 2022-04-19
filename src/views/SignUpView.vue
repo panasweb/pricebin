f94111fb061067014d1477b31a9ebd701d91a090
eric chao <ericjardon@hotmail.com>
2022-04-19 18:26:55 -0500

    add mongodb user creation in signup view form

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -19,84 +19,88 @@
             <div class="submit">
                 <button class="btn btn-primary w-100" type="submit">Crear Cuenta</button>
             </div>
-             <router-link to="/login" class="form-link">¿Ya tienes una cuenta? <span>Ingresa</span></router-link> 
+            <router-link to="/login" class="form-link">¿Ya tienes una cuenta? <span>Ingresa</span></router-link>
         </form>
     </div>
 </template>
 <style scoped>
-    @import '../styles/Form.css';
+@import '../styles/Form.css';
 </style>>
 
 
 
 <script lang="ts">
-    import {
-        defineComponent,
-        ref
-    } from "vue"
-    import {
-        useRouter
-    } from 'vue-router'
-    import {
-        validateNewPassword
-    } from "@/utils/validation"
-    import {
-        newUser
-    } from '../services/auth'
-    import Users from '../models/UserManager'
+import {
+    defineComponent,
+    ref
+} from "vue"
+import {
+    useRouter
+} from 'vue-router'
+import {
+    validateNewPassword
+} from "@/utils/validation"
+import {
+    newUser
+} from '../services/auth'
+import Users from '../models/UserManager'
 
-    export default defineComponent({
+export default defineComponent({
 
-        setup(props, ctx) {
-            const router = useRouter();
+    setup(props, ctx) {
+        const router = useRouter();
 
-            const email = ref < string > ("");
-            const password = ref < string > ("");
-            const confirmPassword = ref < string > ("");
-            const role = ref < string > ("developer")
-            const acceptedTerms = ref < boolean > (false);
-            const passwordError = ref < string > ("");
+        const email = ref<string>("");
+        const password = ref<string>("");
+        const confirmPassword = ref<string>("");
+        const role = ref<string>("developer")
+        const acceptedTerms = ref<boolean>(false);
+        const passwordError = ref<string>("");
 
 
-            function redirect() {
-                console.log("Redirecting")
-                router.push({
-                    name: 'home',
-                    replace: true
-                })
-            }
+        function redirect() {
+            console.log("Redirecting")
+            router.push({
+                name: 'home',
+                replace: true
+            })
+        }
 
-            async function onSubmit() {
-                passwordError.value = validateNewPassword(password.value);
+        async function onSubmit() {
+            passwordError.value = validateNewPassword(password.value);
 
-                if (passwordError.value) {
-                    console.log(passwordError.value)
-                } else if (password.value !== confirmPassword.value) {
-                    console.log("Las contraseñas no son iguales")
+            if (passwordError.value) {
+                console.log(passwordError.value)
+            } else if (password.value !== confirmPassword.value) {
+                console.log("Las contraseñas no son iguales")
+            } else {
+                console.log("Submitted");
+                // Try MongoDB first
+                try {
+                    await Users.create({ email: email.value });
+                } catch (e) {
+                    console.error("Error creating user in DB");
+                    // setAlert("Error intenta de nuevo")
+                    return;
+                }
+                const res = await newUser(email.value, password.value);
+                if (res.error) {
+                    console.error("Algo salió mal:", res.error);
                 } else {
-                    console.log("Submitted");
-                    // Try MongoDB first
-                    await Users.create({email: email.value});
-        
-                    const res = await newUser(email.value, password.value);
-
-                    if (res.error) {
-                        console.error("Algo salió mal:", res.error);
-                    } else {
-                        // Redirigir a Home
-                        redirect()
-                    }
+                    // Redirigir a Home
+                    redirect()
                 }
             }
-
-            return {
-                email,
-                password,
-                confirmPassword,
-                role,
-                acceptedTerms,
-                onSubmit
-            }
         }
-    })
+
+        return {
+            email,
+            password,
+            confirmPassword,
+            role,
+            acceptedTerms,
+            onSubmit
+        }
+    }
+})
 </script>
