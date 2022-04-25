3f9ec62f65e868d188d8b3c21f70557e479a7d0d
eric chao <ericjardon@hotmail.com>
2022-04-24 20:52:21 -0500

    add nspin to login form

--- src/views/LogInView.vue
+++ src/views/LogInView.vue
@@ -1,43 +1,60 @@
 <template>
     <div class="form-wrapper">
-    <form @submit.prevent="onSubmit">
-        <img class="logo" src="../assets/logo.svg" alt="Pricebin logo">
-        <h4 class="subtitle">Inicia sesión</h4>
-        <label for="email">Email:</label>
-        <input type="email" name="email" required v-model="email" />
+        <form @submit.prevent="onSubmit">
+            <img class="logo" src="../assets/logo.svg" alt="Pricebin logo">
+            <h4 class="subtitle">Inicia sesión</h4>
+            <label for="email">Email:</label>
+            <input type="email" name="email" required v-model="email" />
 
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" required v-model="password" />
-        <div class="submit">
-            <button class="btn btn-primary w-100" type="submit">Iniciar Sesión</button>
-        </div>
-                     <router-link to="/register" class="form-link">¿Nuevo en el barrio? <span>Registrate</span></router-link> 
-    </form>
+            <label for="password">Contraseña:</label>
+            <input type="password" name="password" required v-model="password" />
+            <div class="submit">
+
+
+                <button class="btn btn-primary w-100" type="submit">
+                    <n-spin :show="isLoading" size="small" stroke="#fff">
+                        <p>Iniciar Sesión</p>
+                    </n-spin>
+                </button>
+                <n-alert v-show="showAlert" title="Error Text" type="error">
+                    {{ alertMessage }}
+                </n-alert>
+            </div>
+            <router-link to="/register" class="form-link">¿Nuevo en el barrio? <span>Registrate</span></router-link>
+        </form>
     </div>
 </template>
 
 
 <script setup lang="ts">
-
 import { logIn } from "@/services/auth";
 import { ref } from "vue"
 import { useRouter } from "vue-router"
+import { NAlert, NSpin } from "naive-ui";
 const router = useRouter();
 const email = ref<string>("");
 const password = ref<string>("");
+const alertMessage = ref<string>("");
+const showAlert = ref<boolean>(false);
+const isLoading = ref<boolean>(false);
 
 function redirect() {
-    router.push({name: 'home', replace:true});
+    router.push({ name: 'home', replace: true });
 }
 
 async function onSubmit() {
+    if (isLoading.value) return;
     console.log("Logging in", email);
-    
-    const loginResponse = await logIn(email.value, password.value);
+    showAlert.value = false;
+    isLoading.value = true;
 
+    const loginResponse = await logIn(email.value, password.value);
+    isLoading.value = false;
     if (loginResponse.success) {
         redirect();
     } else {
+        showAlert.value = true;
+        alertMessage.value = "Usuario / Contraseña incorrectos"
         console.error("Error logging in", loginResponse.error);
     }
 }
