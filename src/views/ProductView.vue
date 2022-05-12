9facf0c53d36cf8912d3c275a81230f917fe7019
Mau Acosta <chafito@live.com>
2022-05-12 18:57:05 -0500

    Added upvote svg

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -21,7 +21,10 @@
                     </div>
                     <div class="col price">
                         ${{p.amount}}
-                        <div class="priceVotes">Apoyo: {{priceVotes[p._id!]}}</div>
+                        <div class="priceVotes">
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 8C0 3.58065 3.58065 0 8 0C12.4194 0 16 3.58065 16 8C16 12.4194 12.4194 16 8 16C3.58065 16 0 12.4194 0 8ZM9.41935 11.7419V8H11.7065C12.0516 8 12.2258 7.58064 11.9806 7.33871L8.27419 3.65161C8.12258 3.5 7.88065 3.5 7.72903 3.65161L4.01935 7.33871C3.77419 7.58387 3.94839 8 4.29355 8H6.58064V11.7419C6.58064 11.9548 6.75484 12.129 6.96774 12.129H9.03226C9.24516 12.129 9.41935 11.9548 9.41935 11.7419Z" id="unvoted"/></svg>
+                            x{{priceVotes[p._id!]}} 
+                        </div>
                     </div>
                     <button class="add col" @click="addToList(p)">+</button>
 
@@ -217,6 +220,15 @@ span {
     text-align: inherit;
     color: #444;
 }
+
+#unvoted{
+    fill: black;
+}
+
+#voted{
+    fill: #F76D66;
+}
+
 
 @media only screen and (max-width: 700px) {
     .container {
