44efae760898b1ca050ca14c044d94416d676a57
eric chao <ericjardon@hotmail.com>
2022-05-19 18:57:00 -0500

    begin work on store: currentUser for admin rights and currency

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,10 +1,22 @@
+import { User } from '../types/interfaces/User'
 import {reactive} from 'vue'
+import UserManager from '@/models/UserManager';
 
-export const store = reactive({
+interface IStore {
+    currency: string,
+    currentUser?: User,
+}
+
+export const store : IStore = reactive({
     currency:'MXN',
+    currentUser: undefined,
 
     setCurrency(currency:string) {
         this.currency = currency;
+    },
+
+    setCurrentUser(currentUser: User | undefined) {
+        this.currentUser = currentUser;
     }
 })
 
