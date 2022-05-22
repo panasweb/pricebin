06977c1cfb17a9981cc1f8f75688d107d6285028
eric chao <ericjardon@hotmail.com>
2022-05-22 13:31:23 -0500

    add dead search input on ProductListView

--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -1,42 +1,43 @@
 <template>
     <div>
+        <div>
+            <h3>Busca por Nombre:</h3>
+            <n-input placeholder="Flash">
+                <template #prefix>
+                    <n-icon :component="PriceChangeFilled" />
+                </template>
+            </n-input>
+        </div>
         <div class="price-container">
             <ProductCardSquare v-for="p in products" :key="p.name" :product="p" />
         </div>
     </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { defineComponent, onBeforeMount, onMounted, ref } from 'vue'
 import ProductCardSquare from '../components/ProductCardSquare.vue'
-import { exampleProducts } from '@/models/exampleProducts';
 import { Product } from '@/types/interfaces/Product';
 import ProductManager from '@/models/ProductManager';
+import { DeleteForeverRound, PriceChangeFilled } from '@vicons/material'
+import {NInput, NIcon} from "naive-ui"
 
-export default defineComponent({
-    components: {
-        ProductCardSquare
-    },
-    setup() {
+const products = ref<Product[]>([]);
 
-        const products = ref<Product[]>([]);
-        
-        async function fetchProducts() {
-            products.value = await ProductManager.getAll();
-        }
+async function fetchProducts() {
+    products.value = await ProductManager.getAll();
+}
 
-        onBeforeMount(()=>{
-            fetchProducts();
-        })
-
-        return {products};
-    },
+onBeforeMount(() => {
+    fetchProducts();
 })
+
+
 </script>
 
 <style>
 .price-container {
-    width:100%;
+    width: 100%;
     display: flex;
     justify-content: space-between;
     flex-wrap: wrap;
@@ -52,5 +53,4 @@ export default defineComponent({
         align-items: center;
     }
 }
-
 </style>
