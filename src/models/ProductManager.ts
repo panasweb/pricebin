98737ad1ee8b160fe2f1ec3662bcdedfaa666132
eric chao <ericjardon@hotmail.com>
2022-05-25 18:52:59 -0500

    merged Alexis changes and added server error and no results messages on ProductList

--- src/models/ProductManager.ts
+++ src/models/ProductManager.ts
@@ -1,24 +1,21 @@
 import axios from 'axios'
-import {Product} from '@/types/interfaces/Product'
+import { Product } from '@/types/interfaces/Product'
 import Price from '@/types/interfaces/Price'
 
 const url = 'http://localhost:3010/products/';
 
 const ProductManager = {
-    getAll: async function(): Promise<Product[]> {
-        try {
-            const {data} = await axios.get(url); 
-            return data as Product[];
-        } catch (e) {
-            console.error("Error fetching products", e);
-            return []   
-        }
+    getAll: async function (): Promise<Product[]> {
+
+        const { data } = await axios.get(url);
+        return data as Product[];
+
     },
-    create : async function(product: Product) : Promise<[boolean, Product | null]> {
+    create: async function (product: Product): Promise<[boolean, Product | null]> {
         console.log("Create product", product.name, product.brand);
-        
+
         try {
-            const {data} : any = await axios.post(url, product);  // message, newDoc
+            const { data }: any = await axios.post(url, product);  // message, newDoc
             console.log(data);
             return [true, data.newDoc as Product];
         } catch (e) {
@@ -26,29 +23,29 @@ const ProductManager = {
             return [false, null];
         }
     },
-    getProduct : async function(id:string) : Promise<Product | null> {
+    getProduct: async function (id: string): Promise<Product | null> {
         if (!id) {
             console.log('No id provided for product');
             return null;
         }
         try {
-            const {data} = await axios.get(url + id);
+            const { data } = await axios.get(url + id);
             return data as Product;
         } catch (e) {
             console.log("API Error", e);
             return null;
         }
     },
-    addPrice : async function(productId:string, price:Price) : Promise<[boolean, Product | null]> {
+    addPrice: async function (productId: string, price: Price): Promise<[boolean, Product | null]> {
         if (!productId) {
             console.log('No id provided for product');
             return [false, null];
         }
-        
+
         const endpoint = url + 'add-price'
 
         try {
-            const {data} = await axios.post(endpoint, {productId, price});
+            const { data } = await axios.post(endpoint, { productId, price });
             return [true, data.newDoc as Product];
 
         } catch (e) {
@@ -56,7 +53,7 @@ const ProductManager = {
             return [false, null];
         }
     },
-    updatePrice : async function(productId:string, priceId:string, newAmount:number) : Promise<[boolean, Product | null]> {
+    updatePrice: async function (productId: string, priceId: string, newAmount: number): Promise<[boolean, Product | null]> {
         /* 
         FindOneAndUpdate by price id in given product, simply updates the amount.
         A product has one price per Store.
@@ -69,7 +66,7 @@ const ProductManager = {
         const endpoint = url + 'update-price'
 
         try {
-            const {data} = await axios.post(endpoint, {productId, priceId, newAmount});
+            const { data } = await axios.post(endpoint, { productId, priceId, newAmount });
             return [true, data.newDoc as Product];
         } catch (e) {
             console.error("API error", e);
@@ -77,7 +74,7 @@ const ProductManager = {
         }
 
     },
-    searchProducts: async function(query:string) : Promise<Product[]>{
+    searchProducts: async function (query: string): Promise<Product[]> {
         // Search by name for now
         if (!query) {
             return await this.getAll();
@@ -85,14 +82,14 @@ const ProductManager = {
 
         const endpoint = url + "by-name";
         try {
-            const {data} = await axios.post(endpoint, {name: query});
+            const { data } = await axios.post(endpoint, { name: query });
             return data as Product[];
         } catch (e) {
             console.error("API error", e);
             return [];
         }
     },
-    adminDeleteProduct: async function(UserKey:string, productId:string) : Promise<[boolean, string | null]> {
+    adminDeleteProduct: async function (UserKey: string, productId: string): Promise<[boolean, string | null]> {
         if (!UserKey) {
             console.log("No UserKey provided for admin delete")
             return [false, null]
@@ -105,7 +102,7 @@ const ProductManager = {
 
         const endpoint = url + "delete/" + productId
         try {
-            const {data} = await axios.post(endpoint, {UserKey});
+            const { data } = await axios.post(endpoint, { UserKey });
 
             return [true, data as string];
 
@@ -114,7 +111,7 @@ const ProductManager = {
             return [false, "Error deleting product"];
         }
     },
-    adminDeletePrice: async function(UserKey:string, productId:string, priceId:string) : Promise<[boolean, Product | null]> {
+    adminDeletePrice: async function (UserKey: string, productId: string, priceId: string): Promise<[boolean, Product | null]> {
         /* 
         FindOneAndUpdate by product id, then pull from array 
         the subdocument matching priceId.
@@ -138,10 +135,10 @@ const ProductManager = {
         try {
             const reqBody = {
                 UserKey,
-                productId, 
+                productId,
                 priceId
             }
-            const {data} = await axios.post(endpoint, reqBody);
+            const { data } = await axios.post(endpoint, reqBody);
             console.log(data.message);
             return [true, data.newDoc as Product];
         } catch (e) {
