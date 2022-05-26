a5aafb72391d7b85087f25e1863162fc99593d71
Mau Acosta <chafito@live.com>
2022-05-26 17:58:50 -0500

    Added router elements on profile page

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -11,17 +11,10 @@
             </div>
       </div>
   </div>
-  <div class="dropdown show">
-  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown link
-  </a>
-
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>
+  
+  <a class="btn btn-primary btn-lg">Ver mis stats</a>
+  <a class="btn btn-secondary btn-lg">Ver mi historial de listas</a>
+    
 </div>
   
 </template>
@@ -89,5 +82,11 @@ onBeforeMount(() => {
 
 .top img{
     width: 160px;
+}
+
+.btn{
+    display: block;
+    margin: 1rem auto;
+    width: 50%;
 }
 </style>
